c147012ad72b8506aceef8ad4a971d05bbea14c7
sashasergeev <2ndmission3@gmail.com>
2022-02-25 16:22:00 +0300

    Changed vk api wrapper to async one.

--- main.py
+++ main.py
@@ -1,82 +1,62 @@
 import asyncio
 
-from vk_api.keyboard import VkKeyboard, VkKeyboardColor
-from vk_api.longpoll import VkEventType, VkLongPoll
-from vk_api.utils import get_random_id
+from vkbottle.bot import Bot, Message
+from vkbottle import Keyboard, Text, KeyboardButtonColor
 
 from platforms import applemus, spotify, vk, yandex
 import messages
 
 # INIT BOT
-longpoll = VkLongPoll(vk.vk)
+bot = Bot(api=vk.vk)
+
+KEYBOARD = Keyboard(one_time=False).add(Text("Информация"),color=KeyboardButtonColor.PRIMARY).get_json()
 
 
-# BOT INIT DIALOG
-def init_bot(user_id: int) -> None:
-    keyboard = VkKeyboard(one_time=False)
-    keyboard.add_button("Информация",color=VkKeyboardColor.PRIMARY)
-    vk.vk.method('messages.send', {'user_id': user_id,
-                                    'random_id': get_random_id(),
-                                    'message': messages.WELCOME,
-                                    'keyboard': keyboard.get_keyboard()})
-
-
-def send_msg(user_id: int, message: str) -> None:
-    vk.write_msg(user_id, get_random_id(), message)
-
-
-async def attached_to_links(event: VkEventType.MESSAGE_NEW) -> None:
-    # VK ATTECHED AUDIO -> OTHER PLATFORMS
-    audio_id = event.attachments.get('attach1')
-    query = await vk.get_audio_by_id(audio_id)
-    spotify_link, yandex_link = await asyncio.gather(spotify.get_track_by_name(query),
-                                                    yandex.get_track_by_name(query))
-    message = f'Spotify\n{spotify_link}\nYandex Music\n{yandex_link}\nApple Music: В данный момент, сервис недоступен.'
-    vk.write_msg(event.user_id, event.message_id - 1000,
-                                                message)
-
-
-async def links_to_attached(event: VkEventType.MESSAGE_NEW, url: str) -> None:
+async def links_to_attached(url: str) -> None:
     # LINK FROM OTHER PLATFORMS TO VK AUDIO CASE
+    await asyncio.sleep(10)
     if "spotify" in url: track_info = await spotify.get_info_by_url(url)
     elif "apple" in url: track_info = await applemus.get_info_by_url(url)
     elif "yandex" in url: track_info = await yandex.get_info_by_url(url)
-
     if not track_info or track_info == messages.NOT_FOUND:
-        return send_msg(event.user_id, messages.ERROR)
-
-    track_id_vk = await vk.get_track_id(track_info)
-    vk.write_msg(event.user_id, event.message_id - 1000,
-                    "Приятного прослушивания!", track_id_vk)
+        return None
+    return await vk.get_track_id(track_info)
 
 
-async def main():
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            ## TYPING STATUS IN THE DIALOG 
-            vk.vk.method('messages.setActivity', {'type':'typing', 'peer_id':int(event.user_id)})
+@bot.on.message(text="/start")
+async def start(message: Message):
+    # INIT DIALOG
+    await message.answer(messages.WELCOME, keyboard=KEYBOARD)
+
+
+@bot.on.message(text="Информация")
+async def info(message: Message):
+    # GET INFORMATION ABOUT BOT
+    await message.answer(messages.INFO)
+
+
+@bot.on.message(attachment="audio")
+async def attached_to_links(message: Message):
+    # WHEN USER ATTACHES AN VK AUDIO, BOT WILL SEND ITS LINKS FROM SUPPORTED PLATFPRMS
+    audio_data = message.attachments[0].dict()
+    query = f"{audio_data['audio']['artist']} {audio_data['audio']['title']}"
+    spotify_link, yandex_link = await asyncio.gather(spotify.get_track_by_name(query),
+                                                    yandex.get_track_by_name(query))
+    links = f'Spotify\n{spotify_link}\nYandex Music\n{yandex_link}\nApple Music: В данный момент, сервис недоступен.'
+    await message.answer(links)
+
+
+@bot.on.message(attachment="link")
+@bot.on.message(text=r"https://<!>")
+async def link_to_attached(message: Message):
+    # case when link is in attachment
+    try: url = message.attachments[0].dict()['link']['url']
+    # case when link is in message text
+    except IndexError: url = message.text
+    track_id = await links_to_attached(url)
+    if track_id:
+        await message.answer(messages.GOOD, attachment=f"audio{track_id}")
+    else: message.answer(messages.ERROR)
             
-            ## INIT DIALOG WITH USER - ADD INFO BTN
-            if event.text == "/start":
-                init_bot(event.user_id)
-                continue # QUIT
-            elif event.text == "Информация":
-                send_msg(event.user_id, messages.INFO)
-                continue # QUIT
 
-            # GET LINK OR ATTACHED AUDIO ID 
-            is_audio_attached = event.attachments.get('attach1_type') == "audio"
-            url = event.text or event.attachments.get('attach1_url')
-            
-            # WHEN THERE'S NOTHING TO CONVERT
-            if not url and not is_audio_attached:
-                send_msg(event.user_id, messages.ERROR)
-                continue # QUIT
-
-            # VK AUDIO -> PLATFOWM LINKS
-            elif is_audio_attached: await attached_to_links(event)
-            # PLATFORM LINKS -> VK AUDIO
-            elif url: await links_to_attached(event, url)
-            
-
-if __name__ == "__main__": asyncio.run(main())
+bot.run_forever()
--- platforms/vk.py
+++ platforms/vk.py
@@ -1,11 +1,12 @@
 import aiohttp
 import requests
-import vk_api
+
 from decouple import config
+from vkbottle import API
 
 # VK AUTH
 token = config('VK_AUTH_TOKEN')
-vk = vk_api.VkApi(token=token)
+vk = API(token, True)
 
 # VK AUDIO API
 token_audio = config('VK_AUTH_TOKEN_KATE')
@@ -13,18 +14,6 @@ user_agent = config('VK_AUTH_AGENT')
 headers = {'User-Agent': user_agent}
 sess = requests.session()
 sess.headers.update({'User-Agent': user_agent})
-
-
-def write_msg(user_id: int, random_id: int, message: str, song_id: str = None) -> None:
-    if song_id is None:
-        vk.method('messages.send', {'user_id': user_id,
-                                    'random_id': random_id,
-                                    'message': message})
-    else:
-        vk.method('messages.send', {'user_id': user_id,
-                                    'random_id': random_id,
-                                    'message': message,
-                                    'attachment': f'audio{song_id}'})
 
 
 async def get_track_id(searchTrack: str) -> str:
@@ -39,14 +28,14 @@ async def get_track_id(searchTrack: str) -> str:
             return f"{data['owner_id']}_{data['id']}"
 
 
-async def get_audio_by_id(song_id: str) -> str:
-    params=[('access_token', token_audio),
-                ('audios', song_id),
-                ('v', '5.95')]
-    async with aiohttp.ClientSession() as session:
-        url = 'https://api.vk.com/method/audio.getById'
-        async with session.get(url, params=params, headers=headers) as resp:
-            data = (await resp.json())['response'][0]
-            artist = data['artist']
-            title = data['title']
-            return f"{artist} {title}"
+# async def get_audio_by_id(song_id: str) -> str:
+#     params=[('access_token', token_audio),
+#                 ('audios', song_id),
+#                 ('v', '5.95')]
+#     async with aiohttp.ClientSession() as session:
+#         url = 'https://api.vk.com/method/audio.getById'
+#         async with session.get(url, params=params, headers=headers) as resp:
+#             data = (await resp.json())['response'][0]
+#             artist = data['artist']
+#             title = data['title']
+#             return f"{artist} {title}"
